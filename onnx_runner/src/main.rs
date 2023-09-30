ecd9473637a9a62619595e7af51a70fb2412abfb
DIMITRI MASETTA <S306130@studenti.polito.it>
2023-09-30 23:26:13 +0200

    Upd, main

--- onnx_runner/src/main.rs
+++ onnx_runner/src/main.rs
@@ -1,3 +1,9 @@
+use onnx_runner::parser::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the onnx runner!");
+    let onnx_file = "mnist-12";
+    let onnx_model = Parser::extract_from_onnx_file(onnx_file).expect("Error in onnx file parsing");
+    Parser::store_to_json_file(onnx_file, & onnx_model).expect("Error in storing json");
+    let onnx_model_from_json = Parser::extract_from_json_file(onnx_file).expect("Error in json file parsing");
 }
