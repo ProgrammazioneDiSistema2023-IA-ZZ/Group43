73654799091eb24fcf00af6a533e7043f720c375
DIMITRI MASETTA <S306130@studenti.polito.it>
2023-10-03 22:03:43 +0200

    Upd, main

--- onnx_runner/src/main.rs
+++ onnx_runner/src/main.rs
@@ -1,9 +1,23 @@
 use onnx_runner::parser::parser::Parser;
+use onnx_runner::onnx::onnx_graph::OnnxGraph;
 
 fn main() {
     println!("Welcome to the onnx runner!");
+    //test_parser();
+    test_onnx();
+}
+
+fn test_parser() {
+    println!("Test parser");
     let onnx_file = "mnist-12";
     let onnx_model = Parser::extract_from_onnx_file(onnx_file).expect("Error in onnx file parsing");
     Parser::store_to_json_file(onnx_file, & onnx_model).expect("Error in storing json");
-    let onnx_model_from_json = Parser::extract_from_json_file(onnx_file).expect("Error in json file parsing");
+    let _onnx_model_from_json = Parser::extract_from_json_file(onnx_file).expect("Error in json file parsing");
+}
+
+fn test_onnx() {
+    let onnx_file = "mnist-12";
+    let onnx_model = Parser::extract_from_json_file(onnx_file).expect("Error in json file parsing");
+    let onnx = OnnxGraph::from(onnx_model);
+    println!("Graph: \n{:?}", onnx);
 }
