c6282578bf15258416284ab889eabbd00992ca88
DIMITRI MASETTA <S306130@studenti.polito.it>
2023-10-03 22:03:14 +0200

    Add, onnx node and graph

---
+++ onnx_runner/src/onnx/onnx_graph.rs
@@ -0,0 +1,79 @@
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
+use std::collections::hash_map::Entry;
+use std::rc::Rc;
+use std::string::String;
+use crate::onnx::onnx_node::{AddNeighbour, OnnxNode};
+use crate::onnx::temporary_node::{FromRef, TmpOnnxNode};
+// use crate::onnx::onnx_node::{OnnxNode, FunctionNode, InputNode, AddOut};
+// use crate::onnx::onnx_node::OnnxNode::{Function, Input};
+use crate::parser::onnx_model::onnx_proto3::ModelProto;
+
+#[derive(Debug)]
+pub struct OnnxGraph{
+    root_node: Rc<RefCell<OnnxNode>>,
+    secondary_roots: Vec<Rc<OnnxNode>>
+}
+
+impl From<ModelProto> for OnnxGraph{
+    fn from(model: ModelProto) -> Self {
+        let fun_nodes: HashMap<&String, Rc<RefCell<OnnxNode>>> = model.graph.node.iter()
+            .map(|n| (&n.name, Rc::new(RefCell::new(OnnxNode::from(n)))))
+            .collect();
+        let init_nodes: HashMap<&String, Rc<RefCell<OnnxNode>>> = model.graph.initializer.iter()
+            .map(|n| (&n.name, Rc::new(RefCell::new(OnnxNode::from(n)))))
+            .collect();
+        let input_nodes: HashMap<&String, Rc<RefCell<OnnxNode>>> = model.graph.input.iter()
+            .map(|n| (&n.name, Rc::new(RefCell::new(OnnxNode::from(n)))))
+            .collect();
+        let output_nodes: HashMap<&String, Rc<RefCell<OnnxNode>>> = model.graph.output.iter()
+            .map(|n| (&n.name, Rc::new(RefCell::new(OnnxNode::from(n)))))
+            .collect();
+
+        println!("Function nodes");
+        for node in fun_nodes.iter(){
+            println!("{:?} => {:?}", node.0, node.1.borrow())
+        }
+        println!("Initalizer nodes");
+        for node in init_nodes.iter(){
+            println!("{:?} => {:?}", node.0, node.1.borrow())
+        }
+        println!("Input nodes");
+        for node in input_nodes.iter(){
+            println!("{:?} => {:?}", node.0, node.1.borrow())
+        }
+        println!("Output nodes");
+        for node in output_nodes.iter(){
+            println!("{:?} => {:?}", node.0, node.1.borrow())
+        }
+
+        input_nodes[&"Input3".to_string()].borrow_mut().add_output(fun_nodes[&"Convolution28".to_string()].clone());
+        // let root_name = String::from("root");
+        let root = input_nodes[&"Input3".to_string()].clone();
+        OnnxGraph{
+            root_node: root,
+            secondary_roots: Vec::default()
+        }
+    }
+}
+
+// impl From<ModelProto> for OnnxGraph{
+//     fn from(model: ModelProto) -> Self {
+//         fn compute_graph(model: &ModelProto, current_node: &mut OnnxNode, outputs_names: HashSet<&String>) {
+//             for node in model.graph.node.iter() {
+//                 for input in node.input.iter(){
+//                     if outputs_names.contains(input){
+//                         current_node.add_output(OnnxNode::new(& node.name));
+//                     }
+//                 }
+//             }
+//         };
+//         let mut root_node = OnnxNode::new(& model.graph.input[0].name);
+//         root_node.add_output(OnnxNode::new(& model.graph.node[0].name));
+//         compute_graph(&model, &mut *root_node.outputs[0].borrow_mut(), model.graph.node[0].output.iter().collect());
+//         OnnxGraph{
+//             root_node: root_node,
+//             secondary_roots: Vec::default()
+//         }
+//     }
+// }
---
+++ onnx_runner/src/onnx/onnx_node.rs
@@ -0,0 +1,137 @@
+use std::cell::{Cell, RefCell};
+use std::fmt::Error;
+use std::io;
+use std::rc::{Rc, Weak};
+use crate::parser::onnx_model::onnx_proto3::{NodeProto, TensorProto, ValueInfoProto};
+// use crate::onnx::onnx_node::OnnxNode::{Function, Input};
+
+// #[derive(Debug)]
+// pub enum OnnxNode{
+//     Input(InputNode),
+//     Output(OutputNode),
+//     Function(FunctionNode)
+// }
+//
+// #[derive(Debug)]
+// pub struct InputNode{
+//     node_name: String,
+//     outputs: Vec<Rc<OnnxNode>>
+// }
+//
+// impl InputNode{
+//     pub fn new(name: String) -> Self{
+//         InputNode{
+//             node_name: name,
+//             outputs: Vec::default(),
+//         }
+//     }
+// }
+//
+// impl AddOut for InputNode{
+//     fn add_output(&mut self, node: OnnxNode) -> () {
+//         self.outputs.push(Rc::new(node));
+//     }
+// }
+//
+// #[derive(Debug)]
+// pub struct OutputNode{
+//     node_name: String,
+//     inputs: Vec<Weak<OnnxNode>>
+// }
+//
+// impl OutputNode{
+//     pub fn new(name: String) -> Self{
+//         OutputNode{
+//             node_name: name,
+//             inputs: Vec::default(),
+//         }
+//     }
+// }
+//
+// #[derive(Debug)]
+// pub struct FunctionNode{
+//     node_name: String,
+//     inputs: Vec<Weak<OnnxNode>>,
+//     outputs: Vec<Rc<OnnxNode>>
+// }
+//
+// impl FunctionNode{
+//     pub fn new(name: String) -> Self{
+//         FunctionNode{
+//             node_name: name,
+//             inputs: Vec::default(),
+//             outputs: Vec::default(),
+//         }
+//     }
+// }
+
+// trait AddOut{
+//     fn add_output(base: &mut OnnxNode, node: OnnxNode) -> Result<(), Error>{
+//         match base {
+//             Input(i) | Function(i) => Ok(()),
+//             _ => Error
+//         }
+//     }
+// }
+
+#[derive(Debug)]
+pub struct OnnxNode{
+    node_name: String,
+    inputs: Vec<Weak<RefCell<OnnxNode>>>,
+    outputs: Vec<Rc<RefCell<OnnxNode>>>,
+}
+
+impl OnnxNode{
+    fn new(name: & String) -> Self{
+        OnnxNode{
+            node_name: name.to_owned(),
+            inputs: Vec::default(),
+            outputs: Vec::default(),
+        }
+    }
+
+    fn get_name(&self) -> &String{
+        &self.node_name
+    }
+
+    fn has_inputs(&self) -> bool{
+        !self.inputs.is_empty()
+    }
+
+    fn has_outputs(&self) -> bool{
+        !self.outputs.is_empty()
+    }
+}
+
+impl From<& NodeProto> for OnnxNode{
+    fn from(node_proto: & NodeProto) -> Self {
+        OnnxNode::new(& node_proto.name)
+    }
+}
+
+impl From<& ValueInfoProto> for OnnxNode{
+    fn from(value_proto: & ValueInfoProto) -> Self {
+        OnnxNode::new(& value_proto.name)
+    }
+}
+
+impl From<& TensorProto> for OnnxNode{
+    fn from(tensor_proto: & TensorProto) -> Self {
+        OnnxNode::new(& tensor_proto.name)
+    }
+}
+
+pub trait AddNeighbour{
+    fn add_output(&mut self, node:  Rc<RefCell<OnnxNode>>) -> ();
+    fn add_input(&mut self, node:  Rc<RefCell<OnnxNode>>) -> ();
+}
+
+impl AddNeighbour for OnnxNode {
+    fn add_output(&mut self, node: Rc<RefCell<OnnxNode>>) -> () {
+        self.outputs.push(node);
+    }
+
+    fn add_input(&mut self, node: Rc<RefCell<OnnxNode>>) -> () {
+        self.inputs.push(Rc::downgrade(&node));
+    }
+}
